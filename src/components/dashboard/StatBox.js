b7f6637fd50796da1b669c23ed1b30c642cebf78
webadmin <freeman@danfreedesign.com>
2021-09-26 14:10:38 -0400

    Built Structure and style for statBoxes

---
+++ src/components/dashboard/StatBox.js
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const StatBox = (props) => {
+  return (
+    <div className="card statbox">
+      <div className="card-header">
+        <span className="icon-text">
+          <span className="icon">
+            <FontAwesomeIcon size='4x' color={props.color} icon={props.icon} />
+          </span>
+        </span>
+        <div>
+          <p className="light-text">{props.title}</p>
+          <span className="card-header-title">{props.figure}</span>
+        </div>
+      </div>
+      <div className="card-footer">
+        <span className="card-footer-item">Last updated:</span>
+      </div>
+    </div>
+  );
+};
+
+export default StatBox;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,76 @@
 import { Route } from "react-router-dom";
-import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
+import Header from "../components/Header";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGrinStars,
+  faFrown,
+  faGrin,
+  faGrinBeamSweat,
+} from "@fortawesome/free-regular-svg-icons";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+
+import StatBox from "../components/dashboard/StatBox";
 
 const Dashboard = () => {
+  const colorSuccess = "#48c78e";
+  const colorInfo = "#3e8ed0";
+  const colorWarning = "#ffe08a";
+  const colorDanger = "#dd3333";
+
   return (
-    <section>
-      <h1>Dashboard</h1>
-      <BreadcrumbsItem
-        to={`/dashboard`}
-        className="is-active"
-      >{`Dashboard`}</BreadcrumbsItem>
-      <Route path="/dashboard/new-user">
-        <p>Welcome, new user!</p>
-      </Route>
-    </section>
+    <div>
+      <Header />
+      <section>
+        <Route path="/dashboard/new-user">
+          <p>Welcome, new user!</p>
+        </Route>
+      </section>
+      <section className="section">
+        <div className="columns">
+          <div className="column">
+            <div className="block">
+              <StatBox
+                icon={faGrinStars}
+                color={colorInfo}
+                title="Total Contracts"
+                figure="20"
+              />
+            </div>
+          </div>
+          <div className="column">
+            <div className="block">
+              <StatBox
+                icon={faGrin}
+                color={colorSuccess}
+                title="Active Contracts"
+                figure="10"
+              />
+            </div>
+          </div>
+          <div className="column">
+            <div className="block">
+              <StatBox
+                icon={faGrinBeamSweat}
+                color={colorWarning}
+                title="Draft Contracts"
+                figure="5"
+              />
+            </div>
+          </div>
+          <div className="column">
+            <div className="block">
+              <StatBox
+                icon={faFrown}
+                color={colorDanger}
+                title="Expired Contracts"
+                figure="5"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
