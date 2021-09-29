76fa47c42055e626a90ffe49d60db9c64037b6a6
webadmin <freeman@danfreedesign.com>
2021-09-29 19:44:01 -0400

    Added notification sidebar and items. Setup sidebar toggle

--- src/components/UI/Notification.js
+++ src/components/UI/Notification.js
@@ -1,25 +1,49 @@
+import { useEffect } from "react";
+import { useRef } from "react";
+
+import OutsideClick from "../../hooks/outside-click";
+
 const Notification = (props) => {
   let specialClasses = "";
+  //const sidebar = useRef(null);
+  //const outsideSidebar = OutsideClick(sidebar);
 
   if (props.status === "pending") {
-    return (
-      <span className="is-warning">{props.status}</span>
-    )
+    return <span className="is-warning">{props.status}</span>;
   }
   if (props.status === "expired") {
-    return (
-      <span className="is-danger">{props.status}</span>
-    )
+    return <span className="is-danger">{props.status}</span>;
   }
 
+  const sidebarClasses = `${props.isToggled ? "slide-in-left" : "slide-out-right"}`;
+
+  console.log("isToggled is: ", props.isToggled);
+
   return (
-    <aside className="menu-list">
-      <ul>
-        <li>
-          <h2>{props.title}</h2>
-          <p>{props.message}</p>
-        </li>
-      </ul>
+    <aside className={`notification-sidebar card ${sidebarClasses}`}>
+      <div className="card-header">
+        <h5>Recent Notifications</h5>
+        <button onClick={props.onClick} className="delete"></button>
+      </div>
+      <div className="card-content">
+        <ul>
+          <li className="notification-item">
+            <div className="card">
+              <button
+                className="delete"
+              ></button>
+              <div className="card-header">
+                <span className="is-small">Some Title</span>
+                <span className="is-small">2 days ago</span>
+              </div>
+              <div className="card-content">
+                <span className="is-small">Some Title</span>
+                <p className="is-small">Some Message</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </aside>
   );
 };
